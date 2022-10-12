a2d8da26cae7e9688f1551cc0c40418e428b7384
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2022-10-12 08:19:37 +0100

    hello

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;;
+
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner io = new Scanner(System.in);
+      String hi =   io.nextLine();
+      System.out.println(hi.replaceFirst("\\\\i",""));
+    }
+  }
