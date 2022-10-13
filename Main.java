2e31f847378f3aba5a30d813b1c5f846098e7dfb
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2022-10-13 08:19:38 +0100

    i fixed the problem in regex

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
-import java.util.*;;
+import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) {
-      Scanner io = new Scanner(System.in);
-      String hi =   io.nextLine();
-      System.out.println(hi.replaceFirst("\\\\i",""));
+  public static void main(String[] args) {
+      String right = "127.0.0.1:99";
+      String mistake = "999.0.0";
+      String regex = "^([0-9]{1,3}\\.){3}[0-9]{1,3}:[0-9]{1,3}$";
+      //Scanner io = new Scanner(System.in);
+      //String hi =   io.nextLine();
+      System.out.println(right.matches(regex) + " " + mistake.matches(regex) + " \n");
+      //io.close();
     }
   }
