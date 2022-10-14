a918821ab09b46d8d8347f56561995d822d7d130
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2022-10-14 07:38:07 +0100

    hello

--- Main.java
+++ Main.java
@@ -3,12 +3,20 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println("happy birtday")  ; 
+   
+
+
+    }
+    static void exo1()
+    {
+            
       String right = "127.0.0.1:99";
       String mistake = "999.0.0";
-      String regex = "^([0-9]{1,3}\\.){3}[0-9]{1,3}:[0-9]{1,3}$";
-      //Scanner io = new Scanner(System.in);
-      //String hi =   io.nextLine();
+      String regex = "^([0-9]{1,3}\\.){3}[0-9]{1,3}:[0-9]{1,4}$";
+      Scanner io = new Scanner(System.in);
+      String hi =   io.nextLine();
       System.out.println(right.matches(regex) + " " + mistake.matches(regex) + " \n");
-      //io.close();
+      io.close();
     }
   }
